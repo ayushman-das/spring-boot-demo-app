bcf177e29d58e837769b9b6d378cbf442a6a2e91
ayushman-das <75137433+ayushman-das@users.noreply.github.com>
2025-10-23 15:57:56 +0530

    Update beforeinstall.sh
    Fixed come installation changes

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -xe
 cd /home/ec2-user
 sudo rm -rf *
-sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-sudo yum install -y apache-maven
-sudo yum install java-1.8.0-openjdk-devel.x86_64 -y
+sudo yum install -y maven
+sudo yum install java-17-amazon-corretto-devel -y 
 PID=`ps -eaf | grep webapp | grep -v grep | awk '{print $2}'`
 if [[ "" !=  "$PID" ]]; then
   echo "killing $PID"
