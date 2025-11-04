e0f82f9a133f241e3346830f5a6f709b61dfbd06
ayushman-das <75137433+ayushman-das@users.noreply.github.com>
2025-11-04 11:11:49 +0530

    Update beforeinstall.sh
    fixed ubuntu changes

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -3,7 +3,7 @@ cd /home/ubuntu
 sudo rm -rf *
 sudo apt install zip -y
 sudo apt install -y maven
-sudo yum install openjdk-17-jdk -y 
+sudo apt install openjdk-17-jdk -y 
 PID=`ps -eaf | grep webapp | grep -v grep | awk '{print $2}'`
 if [[ "" !=  "$PID" ]]; then
   echo "killing $PID"
